7688435744e5fff21c98045416b356f8abe5ef8a
Hamidul Islam Maruf <hamidulislammaruf2@gmail.com>
2025-06-23 23:15:08 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const lenis = new Lenis();
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
